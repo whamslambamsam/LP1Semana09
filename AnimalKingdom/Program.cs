cdfaa8982b9f71ced41cfc3f1598db18a1cb30e4
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-05-06 18:19:01 +0100

    Added AnimalKingdom project

---
+++ AnimalKingdom/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnimalKingdom
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
