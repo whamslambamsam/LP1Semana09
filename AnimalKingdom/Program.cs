be73940d5222413d190554628151fca18ba301a5
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-05-08 00:18:34 +0100

    Added Program file from Moodle

--- AnimalKingdom/Program.cs
+++ AnimalKingdom/Program.cs
@@ -2,11 +2,48 @@
 
 namespace AnimalKingdom
 {
+    // Alternative Program class if you don't want to program your own
     public class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello LP!");
+            const int n = 10;
+            Animal[] animals = new Animal[n];
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                switch (rnd.Next(4))
+                {
+                    case 0:
+                        animals[i] = new Bat();
+                        break;
+                    case 1:
+                        animals[i] = new Bee();
+                        break;
+                    case 2:
+                        animals[i] = new Cat();
+                        break;
+                    case 3:
+                        animals[i] = new Dog();
+                        break;
+                    default:
+                        // This should NEVER happen
+                        throw new ArgumentOutOfRangeException("Unknown animal!");
+                }
+            }
+
+            foreach (Animal a in animals)
+            {
+                Console.Write($"{a.GetType().Name}{a.Sound()}");
+                if (a is IMammal)
+                    Console.Write(
+                        $"\t| No.Nipples = {(a as IMammal).NumberOfNipples}");
+                if (a is ICanFly)
+                    Console.Write(
+                        $"\t| No.Wings = {(a as ICanFly).NumberOfWings}");
+                Console.WriteLine();
+            }
         }
     }
 }
