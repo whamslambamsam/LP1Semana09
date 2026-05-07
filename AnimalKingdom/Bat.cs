01e35a5cbf1a61aeddd65e1488674a21297adcfb
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-05-07 23:52:43 +0100

    Assigned ICanFly to Bat and Bee scripts

--- AnimalKingdom/Bat.cs
+++ AnimalKingdom/Bat.cs
@@ -1,8 +1,9 @@
 namespace AnimalKingdom
 {
-    public class Bat : Animal, IMammal
+    public class Bat : Animal, IMammal, ICanFly
     {
         public int NumberOfNipples {get; } = 2;
+        public int NumberOfWings {get; } = 2;
         public override string Sound()
         {
             return base.Sound() + "Screech!";
--- AnimalKingdom/Bee.cs
+++ AnimalKingdom/Bee.cs
@@ -1,7 +1,8 @@
 namespace AnimalKingdom
 {
-    public class Bee : Animal
+    public class Bee : Animal, ICanFly
     {
+        public int NumberOfWings {get; } = 4;
         public override string Sound()
         {
             return base.Sound() + "Bzzzzzzz!";
