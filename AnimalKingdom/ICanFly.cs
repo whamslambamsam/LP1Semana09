63a085113b2a5e994a959df18dc3284f1cb12598
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-05-07 23:02:38 +0100

    Fixed error on script

---
+++ AnimalKingdom/ICanFly.cs
@@ -0,0 +1,7 @@
+namespace AnimalKingdom
+{
+    public interface ICanFly
+    {
+        int NumberOfWings {get; }
+    }
+}
---
+++ AnimalKingdom/IMammal.cs
@@ -0,0 +1,7 @@
+namespace AnimalKingdom
+{
+    public interface IMammal
+    {
+        int NumberOfNipples {get; }
+    }
+}
